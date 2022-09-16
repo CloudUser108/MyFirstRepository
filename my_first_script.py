478b0ab5b8fa6a58659e90c44be6568fbd67c33d
CloudUser108 <110035893+CloudUser108@users.noreply.github.com>
2022-09-16 15:26:08 +0530

    Add files via upload

---
+++ my_first_script.py
@@ -0,0 +1,12 @@
+print("Hello World")
+
+number = int(input("enter any number"))
+
+print("the number entered is :",number)
+
+if(number<100):
+    print("the number is within 100")
+elif(number>100 and number<1000):
+    print("the number is within 1000")
+elif(number>1000 and number<2000):
+    print("number us within 2000")
